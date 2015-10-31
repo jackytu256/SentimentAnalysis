d6c10604dc4f3853479b53d13b26fa2b04f574f6
tuhung-te <jacky0938@hotmail.com>
2015-10-31 18:01:29 +1100

    remove import np

--- Web/run.py
+++ Web/run.py
@@ -6,8 +6,9 @@ import time
 import datetime
 from operator import itemgetter
 app = Flask(__name__)
-from DBManager import  DBManager as dbmanager
-from Settings import DBMapping,excluded_Words
+from Tweet_Harvest.DBManager import  DBManager as dbmanager
+from Tweet_Harvest.Settings import DBMapping,excluded_Words
+
 
 db = dbmanager()
 
